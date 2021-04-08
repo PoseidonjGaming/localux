d76f9513fc82e05a3cbd77c86430a696124bf919
PoseidonjGaming <lucasmagnier@free.fr>
2021-04-08 11:50:30 +0200

    form + post

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,6 +11,8 @@ use App\Entity\LocationSansChauffeur;
 use App\Form\LocSansChauffTypeForm;
 use App\Entity\Vehicule;
 use App\Entity\Tarification;
+use App\Entity\Client;
+use Symfony\Component\Validator\Constraints\DateTime;
 
 class HomeController extends AbstractController
 {
@@ -20,6 +22,7 @@ class HomeController extends AbstractController
     public function index(Request $request): Response
     {
         $loc=new LocationSansChauffeur();
+        $em=$this->getDoctrine()->getManager();
         $form=$this->createForm(LocSansChauffTypeForm::class, $loc);
         $form->handleRequest($request);
         if($form->isSubmitted() && $form->isValid()){
@@ -27,10 +30,30 @@ class HomeController extends AbstractController
             $laVoitureInt=array_rand($voitures,1);
             $laVoiture=$this->getDoctrine()->getRepository(Vehicule::class)->findModele($laVoitureInt+1);
             $montant=$this->getDoctrine()->getRepository(Tarification::class)->findWhere($laVoiture->getLeModele(),$form->get('laFormuleSC')->getData()->getLibelle());
+
+            $now=new \DateTime();
+            $prevueD=new \DateTime($_POST['departPrevuD'].$_POST['departPrevuH']);
+            $duree=$form->get('laFormuleSC')->getData()->getDuree();
+            if($duree==4){
+                $prevueR_tmp=new \DateTime($_POST['departPrevuH']);
+                $prevueR_tmp->format('H');
+                $str=strVal(intVal($prevueR_tmp->format('H'))+4).$prevueR_tmp->format('i').$prevueR_tmp->format('s');
+                //if(intVal($_POST['departPrevuH'])<21)
+                $prevueR=new \DateTime($_POST['departPrevuD'].$str);
+                //$loc->set
+            }
             
+
+            $loc->setMontantRegle(floatval($montant->getTarif()));
+            $loc->setLeVehicule($laVoiture);
+           
+            $loc->setDateLocation($now);
+            $loc->setDateHreDepartPrevu($prevueD);
+            $client=$this->getDoctrine()->getRepository(Client::class)->findAll();
+            $loc->setLeClient($client[0]);
             
-            
-            //return $this->redirectToRoute('logins');            
+
+                       
         }
         return $this->render('home/index.html.twig', [
             'form' => $form->createView(),
